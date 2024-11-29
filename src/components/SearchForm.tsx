5cdd25b0ff65499e4f0c74a617bcd28760160f7b
dadamtaylor <derek.taylor@mgramx.com>
2024-11-29 14:44:58 -0700

    Add search functionality with state pushed up. Use children pattern to avoid prop drilling. No pagination yet.

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,21 @@
-export default function SearchForm() {
+export default function SearchForm({ searchText, setSearchText }) {
   return (
-    <form action="#" className="search">
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+      action="#"
+      className="search"
+    >
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
+        value={searchText}
+        onChange={(event) => {
+          setSearchText(event.target.value);
+        }}
         spellCheck="false"
         type="text"
         required
