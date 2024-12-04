a69cdc2000db34d41d1ed1e54acfceb6dea89274
dadamtaylor <derek.taylor@mgramx.com>
2024-12-03 22:06:53 -0500

    Add pagination functionality.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,14 +14,34 @@ import JobList from "./JobList";
 import PaginationControls from "./PaginationControls";
 import { useDebounce, useJobItems } from "../lib/hooks";
 import { Toaster } from "react-hot-toast";
+import { RESULTS_PER_PAGE } from "../lib/constants";
 
 function App() {
   const [searchText, setSearchText] = useState("");
   const debouncedSearchText = useDebounce(searchText, 400);
   const { jobItems, isLoading } = useJobItems(debouncedSearchText);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const totalNumberOfResults = jobItems ? jobItems.length : 0;
-  const jobItemsCurrentPage = jobItems ? jobItems.slice(0, 7) : [];
+  const totalNumberOfPages = Math.ceil(totalNumberOfResults / RESULTS_PER_PAGE);
+  const jobItemsCurrentPage = jobItems
+    ? jobItems.slice(
+        (currentPage - 1) * RESULTS_PER_PAGE,
+        currentPage * RESULTS_PER_PAGE
+      )
+    : [];
+
+  const handleSetSearchText = (searchText: string) => {
+    setCurrentPage(1);
+    setSearchText(searchText);
+  };
+  const handleChangePage = (direction: "next" | "back") => {
+    if (direction === "next") {
+      setCurrentPage((prev) => prev + 1);
+    } else if (direction === "back") {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
 
   return (
     <>
@@ -33,7 +53,10 @@ function App() {
           <BookmarksButton />
         </HeaderTop>
 
-        <SearchForm searchText={searchText} setSearchText={setSearchText} />
+        <SearchForm
+          searchText={searchText}
+          setSearchText={handleSetSearchText}
+        />
       </Header>
 
       <Container>
@@ -43,7 +66,11 @@ function App() {
             <SortingControls />
           </SidebarTop>
           <JobList jobItems={jobItemsCurrentPage} isLoading={isLoading} />
-          <PaginationControls />
+          <PaginationControls
+            currentPage={currentPage}
+            totalNumberOfPages={totalNumberOfPages}
+            onClick={handleChangePage}
+          />
         </Sidebar>
         <JobItemContent />
       </Container>
--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,3 +1,66 @@
-export default function PaginationControls() {
-  return <section className="pagination"></section>;
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+
+type PaginationControlsProps = {
+  currentPage: number;
+  totalNumberOfPages: number;
+  onClick: (direction: "next" | "back") => void;
+};
+
+type PaginationButtonProps = {
+  direction: "back" | "next";
+  currentPage: number;
+  onClick: () => void;
+};
+
+export default function PaginationControls({
+  currentPage,
+  totalNumberOfPages,
+  onClick,
+}: PaginationControlsProps) {
+  return (
+    <section className="pagination">
+      {currentPage > 1 && (
+        <PaginationButton
+          direction="back"
+          currentPage={currentPage}
+          onClick={() => onClick("back")}
+        />
+      )}
+      {currentPage < totalNumberOfPages && (
+        <PaginationButton
+          direction="next"
+          currentPage={currentPage}
+          onClick={() => onClick("next")}
+        />
+      )}
+    </section>
+  );
+}
+
+function PaginationButton({
+  direction,
+  currentPage,
+  onClick,
+}: PaginationButtonProps) {
+  return (
+    <button
+      onClick={(event) => {
+        onClick();
+        event.currentTarget.blur();
+      }}
+      className={`pagination__button pagination__button--${direction}`}
+    >
+      {direction === "back" ? (
+        <>
+          <ArrowLeftIcon />
+          Page {currentPage - 1}
+        </>
+      ) : (
+        <>
+          <ArrowRightIcon />
+          Page {currentPage + 1}
+        </>
+      )}
+    </button>
+  );
 }
