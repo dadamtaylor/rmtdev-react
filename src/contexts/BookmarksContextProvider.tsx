c4dec4b2b0576e01b19db4cd8a72b352281cf495
dadamtaylor <derek.taylor@mgramx.com>
2024-12-05 21:43:56 -0500

    Create our own generic local storage hook and use it.

--- src/contexts/BookmarksContextProvider.tsx
+++ src/contexts/BookmarksContextProvider.tsx
@@ -1,4 +1,5 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext } from "react";
+import { useLocalStorage } from "../lib/hooks";
 
 type BookmarksContext = {
   bookmarkedIds: number[];
@@ -12,21 +13,18 @@ export default function BookmarksContextProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const [bookmarkedIds, setBookmarkedIds] = useState<number[]>(() =>
-    JSON.parse(localStorage.getItem("bookmarkedIds") || "[]")
+  const [bookmarkedIds, setBookmarkedIds] = useLocalStorage<number[]>(
+    "bookmarkedIds",
+    []
   );
 
   const handleToggleBookmark = (id: number) => {
     if (bookmarkedIds.includes(id)) {
-      setBookmarkedIds((prev) => prev.filter((i) => i !== id));
+      setBookmarkedIds((prev) => prev.filter((eachid) => eachid !== id));
     } else {
       setBookmarkedIds((prev) => [...prev, id]);
     }
   };
-
-  useEffect(() => {
-    localStorage.setItem("bookmarkedIds", JSON.stringify(bookmarkedIds));
-  }, [bookmarkedIds]);
 
   return (
     <BookmarksContext.Provider
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -97,3 +97,18 @@ export function useActiveId() {
   }, []);
   return activeId;
 }
+
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [value, setValue] = useState<T>(() =>
+    JSON.parse(localStorage.getItem(key) || JSON.stringify(initialValue))
+  );
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue] as const;
+}
