4ef5b3dff9bdb231e1c9547130397b7ceee831f0
dadamtaylor <derek.taylor@mgramx.com>
2024-12-06 20:40:36 -0500

    Remove popover by registering a click handler with the DOM document class. Restrict it by type of element and class of element.

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,9 +1,29 @@
 import { TriangleDownIcon } from "@radix-ui/react-icons";
 import BookmarksPopover from "./BookmarksPopover";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
+
+  // Add a root node event listener to close the popover when clicking outside
+  // the popover
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      if (
+        event.target instanceof HTMLElement &&
+        !event.target.closest(".bookmarks-btn") &&
+        !event.target.closest(".bookmarks-popover")
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
+
   return (
     <section>
       <button
